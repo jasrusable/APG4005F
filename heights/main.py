133554c26e364182e04677251a5c783af3af2f56
Jason Russell <jasrusable@gmail.com>
2015-05-12 20:03:46 +0200

    work on LS

--- heights/main.py
+++ heights/main.py
@@ -2,25 +2,55 @@
 
 
 class Point(object):
-	def __init__(self, name, z):
+	def __init__(self, name, type_, x, y, z):
 		self.name = name
+		self.type_ = type_
+		self.x = x
+		self.y = y
 		self.z = z
 
-class HeightObservation(object):
-	def __init__(self, from, to, obs_value):
+class Observation(object):
+	def __init__(self, from_, to, type_, value):
 		self.from_ = from_
 		self.to = to
-		self.obs_value = obs_value
+		self.value = value
+		self.type_ = type_
 
-def read_obs(filename='obs.txt'):
-	observations = list()
-	with open(filename, 'r') as f:
+def read_points(filepath='points.txt', delim=','):
+	points = list()
+	with open (filepath, 'r') as f:
 		for line in f:
-			parts = line.split(delim)
+			if line.startswith('#'):
+				continue
+			line_parts = line.split(delim)
+			name = line_parts[0]
+			type_ = line_parts[1]
+			x = line_parts[2]
+			y = line_parts[3]
+			z = line_parts[4]
+			points.append(Point(name, type_, x, y, z))
+	return points
+
+def read_observations(type_, known_points, filepath='obs.txt', delim=','):
+	assert(type_ in ['distance', 'direction'])
+	observations = list()
+	known_point_names = list()
+	for point in known_points:
+		known_point_names.append(point.name)
+	with open(filepath, 'r') as f:
+		for line in f:
+			if line.startswith('#'):
+				continue
+			line_parts = line.split(delim)
 			from_ = parts[0]
+			if from_ not in known_point_names:
+				raise Exception('{0} not defined in known_points.'.format(from_))
 			to = parts[1]
-			obs_value = parts[2]
-			obs = HeightObservation(from_, to, obs_value)
-			observations.append(obs)
+			if to not in known_point_names:
+				raise Exception('{0} not defined in known_points.'.format(to))
+			value = parts[2]
+			observations.append(Observation(from_, to, type_, value))
 	return observations
 
+points = read_points(filepath='points.txt')
+read_observations(type_='distance', known_points=points, filepath='dist_obs.txt')
