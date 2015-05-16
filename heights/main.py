740a343e03faeefcf5399ace9b04c06b580219d7
Jason Russell <jasrusable@gmail.com>
2015-05-16 13:08:00 +0200

    dunno

--- heights/main.py
+++ heights/main.py
@@ -11,21 +11,23 @@ points = read_points(filepath='data/points.txt')
 #observations = read_observations(type_='distance', known_points=points, filepath='data/dist_obs.txt')
 observations = read_observations(type_='direction', known_points=points, filepath='data/dir_obs.txt')
 
-def get_model():
-    from_x = sympy.Symbol('from_x')
-    from_y = sympy.Symbol('from_y')
-    from_z = sympy.Symbol('from_z')
-    to_x = sympy.Symbol('to_x')
-    to_y = sympy.Symbol('to_y')
-    to_z = sympy.Symbol('to_z')
-    orientation_correction = sympy.Symbol('orientation_correction')
-    true = sympy.Symbol('true')
-    sin = sympy.sin
-    cos = sympy.cos
-    tan = sympy.tan
-    atan = sympy.atan
-    hz_direction = atan((to_y - from_y)/(to_x - from_x)) - orientation_correction - true
-    return hz_direction
+def get_model(model):
+    wanted_model = None
+    if model == 'hz_direction':
+        from_x = sympy.Symbol('from_x')
+        from_y = sympy.Symbol('from_y')
+        from_z = sympy.Symbol('from_z')
+        to_x = sympy.Symbol('to_x')
+        to_y = sympy.Symbol('to_y')
+        to_z = sympy.Symbol('to_z')
+        orientation_correction = sympy.Symbol('orientation_correction')
+        true = sympy.Symbol('true')
+        sin = sympy.sin
+        cos = sympy.cos
+        tan = sympy.tan
+        atan = sympy.atan
+        wanted_model = atan((to_y - from_y)/(to_x - from_x)) - orientation_correction - true
+    return wanted_model
 
 def get_A_row(model, observation, points):
     a_row = list()
@@ -39,5 +41,5 @@ def get_A_row(model, observation, points):
             if point == observation.from_:
                 print('is from point')
 
-get_A_row(get_model(), observations[0], points)
-get_A_row(get_model(), observations[4], points)
+get_A_row(get_model('hz_direction'), observations[0], points)
+get_A_row(get_model('hz_direction'), observations[4], points)
